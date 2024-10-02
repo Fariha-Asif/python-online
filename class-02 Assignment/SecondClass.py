26c724f462777c4b474489eb088fbdb0c5435f89
Fariha Asif <163842739+Fariha-Asif@users.noreply.github.com>
2024-10-02 20:52:29 +0500

    Add files via upload

---
+++ class-02 Assignment/SecondClass.py
@@ -0,0 +1,164 @@
+name: str = "Fariha"
+print(name)
+
+age: int = 24
+print(age)
+
+f_name: str = "Muhammad Asif Qureshi"
+print(f_name)
+
+friend_1: str = "Faraz"
+print(friend_1)
+
+percentage: float = 78.88
+print(percentage)
+
+girl_name_1: str = "urooj"
+print(girl_name_1)
+
+girl_name_2: str = "Fatima"
+print(girl_name_2)
+
+girl_name_3: str = "falak"
+print(girl_name_3)
+
+girl_name_4: str = "dua"
+print(girl_name_4)
+
+girl_name_5: str = "muskan"
+print(girl_name_5)
+
+girl_name_6: str = "farisha"
+print(girl_name_6)
+
+girl_name_7: str = "laiba"
+print(girl_name_7)
+
+girl_name_8: str = "sadaf"
+print(girl_name_8)
+
+girl_name_9: str = "misbah"
+print(girl_name_9)
+
+girl_name_10: str = "mariam"
+print(girl_name_10)
+
+girl_name_11: str = "mehak"
+print(girl_name_11)
+
+girl_name_12: str = "mahnoor"
+print(girl_name_12)
+
+girl_name_13: str = "mariya"
+print(girl_name_13)
+
+girl_name_14: str = "palwasha"
+print(girl_name_14)
+
+girl_name_15: str = "khadija"
+print(girl_name_15)
+
+girl_name_16: str = "hira"
+print(girl_name_16)
+
+girl_name_17: str = "hania"
+print(girl_name_17)
+
+girl_name_18: str = "anjum"
+print(girl_name_18)
+
+girl_name_19: str = "areeba"
+print(girl_name_19)
+
+girl_name_20: str = "atiqa"
+print(girl_name_20)
+
+girl_name_21: str = "kulsoom"
+print(girl_name_21)
+
+girl_name_22: str = "kainat"
+print(girl_name_22)
+
+girl_name_23: str = "sitara"
+print(girl_name_23)
+
+girl_name_24: str = "hoor"
+print(girl_name_24)
+
+girl_name_25: str = "hooriya"
+print(girl_name_25)
+
+girl_name_26: str = "hadiya"
+print(girl_name_26)
+
+girl_name_27: str = "maham"
+print(girl_name_27)
+
+girl_name_28: str = "maheen"
+print(girl_name_28)
+
+girl_name_29: str = "mano"
+print(girl_name_29)
+
+girl_name_30: str = "bubbly"
+print(girl_name_30)
+
+girl_name_31: str = "irha"
+print(girl_name_31)
+
+girl_name_32: str = "isha"
+print(girl_name_32)
+
+girl_name_33: str = "zubia"
+print(girl_name_33)
+
+girl_name_34: str = "fajar"
+print(girl_name_34)
+
+girl_name_35: str = "hamna"
+print(girl_name_35)
+
+girl_name_36: str = "amina"
+print(girl_name_36)
+
+girl_name_37: str = "arisha"
+print(girl_name_37)
+
+girl_name_38: str = "urooj"
+print(girl_name_38)
+
+girl_name_39: str = "abeeha"
+print(girl_name_39)
+
+girl_name_40: str = "ayesha"
+print(girl_name_40)
+
+girl_name_41: str = "abidah"
+print(girl_name_41)
+
+girl_name_42: str = "abeer"
+print(girl_name_42)
+
+girl_name_43: str = "neha"
+print(girl_name_43)
+
+girl_name_44: str = "akifah"
+print(girl_name_44)
+
+girl_name_45: str = "anabiya"
+print(girl_name_45)
+
+girl_name_46: str = "abeeha"
+print(girl_name_46)
+
+girl_name_47: str = "barirah"
+print(girl_name_47)
+
+girl_name_48: str = "mubashira"
+print(girl_name_48)
+
+girl_name_49: str = "asma"
+print(girl_name_49)
+
+girl_name_50: str = "eeman"
+print(girl_name_50)
