c9cb40ee92d3c09228ae3db5a838c49b848a374e
Fariha Asif <163842739+Fariha-Asif@users.noreply.github.com>
2024-10-02 20:39:32 +0500

    Add files via upload

---
+++ class-01-Assignment/FirstClass.py
@@ -0,0 +1,103 @@
+# Variables:
+
+print("My Name is Fariha.")
+print("I am 24 years old.")
+print("I have started my journey of coding 4months ago.")
+print("I am a student of GIAIC")
+print("Also I have enrolled in PIAIC")
+print("I have completed my inter.")
+print("I want to be data scientist.")
+print("My favourite hobby is cooking and also learn new things to make.")
+print("I am good at football and cricket")
+print("I love explore new things in my life.")
+print("My favourite color is Red and pink.")
+print("My favourite poet is Allama Iqbal.")
+print("I born in Lahore")
+print("Now I live in Karachi.")
+print("We came to karachi 20 years ago")
+print("I like to watch cartoons in my free time.")
+print("I can make any type of dish.")
+print("I like chinese food and pakistani fast food.")
+print("I ove to go for outing")
+print("I don't ike to do shopping")
+print("I go outside just to eat something")
+print("I ike to explore different restaurant taste.")
+print("Whenever i feel bad i like cooking at that time")
+
+name: str = "Fariha"
+print(name)
+
+age: int = 24
+print(age)
+
+favourite_personality: str = "Allama Iqbal"
+print(favourite_personality)
+
+famous_quote: str = "The ultimate aim of the ego is not to see something, but to be something."
+print(famous_quote)
+
+famous_quote_1: str = "The wing of the Falcon brings to the king, the wing if the crow brings him to the cemetery."
+print(famous_quote_1)
+
+famous_quote_2: str = "People who have no hold over their process of thinking are likely to be ruined by liberty of thought. If thought is immature, liberty of thought becomes a method of converting men into animals."
+print(famous_quote_2)
+
+famous_quote_3: str = "Vision without power does bring moral elevation but cannot give a lasting culture."
+print(famous_quote_3)
+
+famous_quote_4: str = "Become dust - and they will throw thee in the air; Become stone - and they will throw thee on glass."
+print(famous_quote_4)
+
+famous_quote_5: str = "When truth has no burning, then it is philosophy, when it gets burning from the heart, it becomes poetry."
+print(famous_quote_5)
+
+famous_quote_6: str = "The scientific observer of Nature is a kind of mystic seeker in the act of prayer."
+print(famous_quote_6)
+
+famous_quote_7: str = "If faith is lost, there is no security and there is no life for him who does not adhere to religion."
+print(famous_quote_7)
+
+famous_quote_8: str = "I have seen the movement of the sinews of the sky, And the blood coursing in the veins of the moon."
+print(famous_quote_8)
+
+famous_quote_9: str = "Islam is itself destiny and will not suffer destiny."
+print(famous_quote_9)
+
+famous_quote_10: str = "Thou art not for the earth, nor for the Heaven the world is for thee, thou art not for the world."
+print(famous_quote_10)
+
+famous_quote_11: str = "Rise above sectional interests and private ambitions... Pass from matter to spirit. Matter is diversity; spirit is light, life and unity."
+print(famous_quote_11)
+
+famous_quote_12: str = "God is not a dead equation!"
+print(famous_quote_12)
+
+famous_quote_13: str = "It is the nature of the self to manifest itself, In every atom slumbers the might of the self."
+print(famous_quote_13)
+
+famous_quote_14: str = "The soul is neither inside nor outside the body; neither proximate to nor separate from it."
+print(famous_quote_14)
+
+famous_quote_15: str = "Though the terror of the sea gives to none security, in the secret of the shell. Self preserving we may dwell."
+print(famous_quote_15)
+
+famous_quote_16: str = "The Ego is partly free. partly determined, and reaches fuller freedom by approaching the Individual who is most free: God."
+print(famous_quote_16)
+
+famous_quote_17: str = "I, therefore, demand the formation of a consolidated Muslim State in the best interest of India and Islam."
+print(famous_quote_17)
+
+famous_quote_18: str = "Conduct, which involves a decision of the ultimate fate of the agent cannot be based on illusions."
+print(famous_quote_18)
+
+famous_quote_19: str = "Art: If the object of poetry is, to make men, then poetry is the heir of prophecy."
+print(famous_quote_19)
+
+famous_quote_20: str = "If the object of poetry is, to make men, then poetry is the heir of prophecy."
+print(famous_quote_20)
+
+famous_quote_21: str = "Plants and minerals are bound to predestination. The faithful is only bound to the Divine orders."
+print(famous_quote_21)
+
+famous_quote_22: str = "Unbeliever is he who follows predestination even if he be Muslim, Faithful is he, if he himself is the Divine Destiny."
+print(famous_quote_22)
