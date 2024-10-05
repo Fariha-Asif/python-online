5875502fa27d4fc982c9f30d0f05c53540632c49
Fariha Asif <163842739+Fariha-Asif@users.noreply.github.com>
2024-10-05 23:46:36 +0500

    Add files via upload

---
+++ class-03 Assignment/class.py
@@ -0,0 +1,197 @@
+quote_1: str = ''' "Shakespeare said:" 'This above all: to thine own self be true.' '''
+print(quote_1)
+
+quote_2: str = ''' "Shakespeare said:" 'Self-love, my liege, is not so vile a sin, as self-neglecting.' '''
+print(quote_2)
+
+quote_3: str = ''' "Shakespeare said:" 'Our doubts are traitors, And make us lose the good we oft might win, By fearing to attempt.' '''
+print(quote_3)
+
+quote_4: str = ''' "Shakespeare said:" 'There is nothing either good or bad, but thinking makes it ' '''
+print(quote_4)
+
+quote_5: str = ''' "Shakespeare said:" 'I do love nothing in the world so well as you: is not that strange?' '''
+print(quote_5)
+
+quote_6: str = ''' "Shakespeare said:" 'The course of true love never did run smooth.' '''
+print(quote_6)
+
+quote_7: str = ''' "Shakespeare said:" 'Love looks not with the eyes, but with the mind; and therefore is winged Cupid painted blind.' '''
+print(quote_7)
+
+quote_8: str = ''' "Shakespeare said:" 'But love is blind, and lovers cannot see.' '''
+print(quote_8)
+
+quote_9: str = ''' "Shakespeare said:" 'If I be waspish, best beware of my sting' '''
+print(quote_9)
+
+quote_10: str = ''' "Shakespeare said:" 'Do you not know I am a woman? When I think, I must speak.' '''
+print(quote_10)
+
+quote_11: str = ''' "Shakespeare said:" 'We know what we are, but know not what we may be.' '''
+print(quote_11)
+
+quote_12: str = ''' "Shakespeare said:" 'Art thou afeard to be the same in thine own act and valour As thou are in desire?' '''
+print(quote_12)
+
+quote_13: str = ''' "Shakespeare said:" 'Diseased Nature oftentimes breaks forth In strange eruptions.' '''
+print(quote_13)
+
+quote_14: str = ''' "Shakespeare said:" 'O, it is excellent To have a giant's strength; but it is tyrannous To use it like a giant.' '''
+print(quote_14)
+
+quote_15: str = ''' "Shakespeare said:" 'Better three hours too soon than a minute late.' '''
+print(quote_15)
+
+quote_16: str = ''' "Shakespeare said:" 'Good company, good wine, good welcome can make good people' '''
+print(quote_16)
+
+name_a: str = '''
+Ayesha
+      Anabiya
+             Ayat
+                 Arzoo
+                       Aiza
+                    Ayra
+              Abeeha
+         Anam
+    Amyra
+Afreen '''
+print(name_a)
+
+name_b: str = '''
+Bisma
+      Bareera
+             Bano
+                 Binish
+                       Batool
+                    Bint
+              Banu
+         Bahar
+    Beena
+Bani '''
+print(name_b)
+
+name_c: str = '''
+Chaman
+      charmi
+             chanchal
+                    chameli
+                           Carmen
+                    caitlyn
+              carmen
+        chasmis
+    chandra
+ciji '''
+print(name_c)
+
+name_d: str = '''
+Darina
+      Dafiyah
+             Dahab
+                  Daiba
+                        Damitha
+                   Damira
+              Dania
+          Damish
+    Danishara
+Danayah '''
+print(name_d)
+
+
+name_e: str = '''
+Eiffat
+      Eillan
+             Eila
+                  Eiman
+                        Elham
+                   Elisha
+              Eshal
+          Eshna
+    Emi
+Esra '''
+print(name_e)
+
+
+name_f: str = '''
+Fariha
+      Faraz
+            Faham
+                  Falak
+                        Farisha
+                  Farishta
+            Fariz
+       Fairy
+    Fari
+Fairy '''
+print(name_f)
+
+
+name_g: str = '''
+Galiba
+      Ghaniya
+            Ghania
+                  Galiba
+                        Gauhar
+                   Gawahir
+             Ghazal
+        Ghazia
+    Ghaania
+Gufrana '''
+print(name_g)
+
+
+name_h: str = '''
+Haniya
+      Hadiya
+            Hiba
+                Hina
+                    Hunzala
+                 Hira
+            Hooriya
+        Haseen
+    Hoor
+Hala '''
+print(name_h)
+
+
+name_i: str = '''
+Iraj
+    Iman
+        Insaf
+             Idonia
+                   Ibna
+               Idna
+           Iffat
+       Ifra
+   Irfa
+Idha '''
+print(name_i)
+
+
+name_j: str = '''
+Jaheen
+      jabeela
+            jazeera
+                    jabb
+                        jalpari
+                    jala
+               Jaimin
+         jaella
+   Jaylin
+Jalwa '''
+print(name_j)
+
+
+name_k: str = '''
+Kainat
+      kaneez
+            kameela
+                    kanwal
+                          kanza
+                    karma
+               kaida
+           karo
+     kasam
+kasia '''
+print(name_k)
